8cb6c7e92dd57a513981c6f34516229e41a29f7e
slowpeek <slowpeek@users.noreply.github.com>
2023-11-26 18:11:26 +0200

    run-docker wrapper

---
+++ run-docker.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+bye () {
+    echo "$@"
+    exit 1
+}
+
+self=$(realpath "$0")
+self_dir=${self%/*}
+self_name=${self##*/}
+
+[[ -n $1 ]] || bye "Usage: ${self_name} <jtv file> [options]"
+[[ -e $1 ]] || bye "no such file: '$1'" 
+[[ -f $1 ]] || bye "'$1' is not a regular file"
+
+jtv=$(realpath "$1")
+shift
+
+opt=(
+    -it --rm --init
+    -v "$jtv":/workdir/jtv.zip:ro
+    -v "${self_dir}/jtv2xml.py":/workdir/script.py:ro
+    -w /workdir
+    python:alpine
+    sh -c "python3 script.py ${*@Q} < jtv.zip"
+)
+
+docker run "${opt[@]}"
